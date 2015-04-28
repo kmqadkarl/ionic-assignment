872c9adf34a8681cd76e53083bffa642909be402
kmqadkarl <kmqad@hotmail.com>
2015-04-28 21:29:23 +0100

    Added styling, added new page add.html, function to add new foods, changed menu

--- www/js/app.js
+++ www/js/app.js
@@ -13,69 +13,201 @@ angular.module('calorific', ['ionic','calorific.controllers'])
     }
   });
 })
+
 // controls the states and how the app is navigates throughout
 .config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
-//state of the menu to the left of the landing page.
+  //state of the menu to the left of the landing page.
   .state('app', {
     url: "/app",
     abstract: true,
     templateUrl: "templates/menu.html",
 
   })
-// breakfast html
+  // breakfast html
   .state('app.breakfast', {
-  url: "/breakfast",
-  views: {
-    'menuContent': {
-      templateUrl: "templates/breakfast.html",
+    url: "/breakfast",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/breakfast.html",
 
+      }
+    }
+  })
+  //lunch html
+  .state('app.lunch', {
+    url: "/lunch",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/lunch.html",
+
+      }
+    }
+  })
+  //dinner html
+  .state('app.dinner', {
+    url: "/dinner",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/dinner.html",
+
+      }
+    }
+  })
+  //about the apphtml
+  .state('app.about', {
+    url: "/about",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/about.html",
+
+      }
+    }
+  })
+
+  //add the apphtml
+  .state('app.add', {
+    url: "/add",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/add.html",
+
+      }
+    }
+  })
+  //main landing page where the app starts from
+  .state('app.home', {
+    url: "/home",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/home.html",
+
+      }
+    }
+  })
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
+
+
+})
+
+.factory('Foods', function() {
+  return {
+    all: function() {
+      var foodString = window.localStorage['foods'];
+      if (foodString) {
+        return angular.fromJson(foodString);
+      }
+      return [];
+    },
+    save: function(foods) {
+      window.localStorage['foods'] = angular.toJson(foods);
+    },
+    newFood: function(foodTitle) {
+      // Add a new project
+      return {
+        title: foodTitle,
+        title: foodTitle,
+        tasks: []
+      };
+    },
+    getLastActiveIndex: function() {
+      return parseInt(window.localStorage['lastActiveFood']) || 0;
+    },
+    setLastActiveIndex: function(index) {
+      window.localStorage['lastActiveFood'] = index;
     }
   }
 })
-//lunch html
-.state('app.lunch', {
-url: "/lunch",
-views: {
-  'menuContent': {
-    templateUrl: "templates/lunch.html",
 
+.controller('TodoCtrl', function($scope, $timeout, $ionicModal, Foods) {
+
+  // A utility function for creating a new food
+  // with the given foodTitle
+  var createFood = function(foodTitle) {
+    var newFood = Foods.newFood(foodTitle);
+    $scope.foods.push(newFood);
+    Foods.save($scope.foods);
+    $scope.selectFood(newFood, $scope.foods.length - 1);
   }
-}
-})
-//dinner html
-.state('app.dinner', {
-url: "/dinner",
-views: {
-  'menuContent': {
-    templateUrl: "templates/dinner.html",
 
+
+  // Load or initialize food
+  $scope.foods = Foods.all();
+
+  // Grab the last active, or the first food
+  $scope.activeFood = $scope.foods[Foods.getLastActiveIndex()];
+
+  // Called to create a new food
+  $scope.newFood = function() {
+    var foodTitle = prompt('Food Type');
+    if (foodTitle) {
+      createFood(foodTitle);
+    }
+  };
+
+  // Called to select the given food
+  $scope.selectFood = function(food, index) {
+    $scope.activeFood = food;
+    Foods.setLastActiveIndex(index);
+    $scope.sideMenuController.close();
+  };
+
+  $scope.completionChanged = function() {
+    Foods.save($scope.foods);
+  };
+
+  // Create our modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    focusFirstInput: false,
+    scope: $scope
+  });
+
+  $scope.createTask = function(task) {
+    if (!$scope.activeFood) {
+      return;
+    }
+    $scope.activeFood.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+
+    // Inefficient, but save all the foods
+    Foods.save($scope.foods);
+
+    task.title = "";
+
+  };
+
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
   }
-}
-})
-//about the apphtml
-.state('app.about', {
-url: "/about",
-views: {
-  'menuContent': {
-    templateUrl: "templates/about.html",
 
-  }
-}
-})
-//main landing page where the app starts from
-.state('app.home', {
-url: "/home",
-views: {
-  'menuContent': {
-    templateUrl: "templates/home.html",
+  $scope.toggleFood = function() {
+    $scope.sideMenuController.toggleLeft();
+  };
 
-  }
-}
-})
 
-// if none of the above states are matched, use this as the fallback
-$urlRouterProvider.otherwise('/app/home');
-
+  // Try to create the first food, make sure to defer
+  // this by using $timeout so everything is initialized
+  // properly
+  $timeout(function() {
+    if ($scope.foods.length == 0) {
+      while (false) {
+        var foodTitle = prompt('Your Food Type:');
+        if (foodTitle) {
+          createFood(foodTitle);
+          break;
+        }
+      }
+    }
+  });
 
 });
